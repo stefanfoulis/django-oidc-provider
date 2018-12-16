c1277ecc6007ae4f5ee3c91487b085868e74e4de
Stefan Foulis <stefan@foulis.ch>
2018-12-16 15:24:32 +0100

    Replace the ResponseType model with a field
    Instead of using a ResponseType model and a ManyToMany relationship to Client, use a custom field that saves the selected response_types in a JSONField.
    This simplifies the model structure and doesn't require keeping the db in-sync with code, since the response_types are a static set of choices anyway.

--- oidc_provider/models.py
+++ oidc_provider/models.py
@@ -8,6 +8,7 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
+from oidc_provider.fields import JSONMultiSelectModelField
 
 CLIENT_TYPE_CHOICES = [
     ('confidential', 'Confidential'),
@@ -29,30 +30,6 @@ JWT_ALGS = [
 ]
 
 
-class ResponseTypeManager(models.Manager):
-    def get_by_natural_key(self, value):
-        return self.get(value=value)
-
-
-class ResponseType(models.Model):
-    objects = ResponseTypeManager()
-
-    value = models.CharField(
-        max_length=30,
-        choices=RESPONSE_TYPE_CHOICES,
-        unique=True,
-        verbose_name=_(u'Response Type Value'))
-    description = models.CharField(
-        max_length=50,
-    )
-
-    def natural_key(self):
-        return self.value,  # natural_key must return tuple
-
-    def __str__(self):
-        return u'{0}'.format(self.description)
-
-
 class Client(models.Model):
 
     name = models.CharField(max_length=100, default='', verbose_name=_(u'Name'))
@@ -68,7 +45,8 @@ class Client(models.Model):
                     u' of their credentials. <b>Public</b> clients are incapable.'))
     client_id = models.CharField(max_length=255, unique=True, verbose_name=_(u'Client ID'))
     client_secret = models.CharField(max_length=255, blank=True, verbose_name=_(u'Client SECRET'))
-    response_types = models.ManyToManyField(ResponseType)
+    response_types = JSONMultiSelectModelField(
+        choices=RESPONSE_TYPE_CHOICES, verbose_name=_('Response Types'))
     jwt_alg = models.CharField(
         max_length=10,
         choices=JWT_ALGS,
@@ -122,11 +100,20 @@ class Client(models.Model):
         return self.__str__()
 
     def response_type_values(self):
-        return (response_type.value for response_type in self.response_types.all())
+        # Return the allowed response types in the same order as they appear in
+        # RESPONSE_TYPE_CHOICES.
+        return [
+            code
+            for code, description in RESPONSE_TYPE_CHOICES
+            if code in self.response_types
+        ]
 
     def response_type_descriptions(self):
-        # return as a list, rather than a generator, so descriptions display correctly in admin
-        return [response_type.description for response_type in self.response_types.all()]
+        response_type_dict = dict(RESPONSE_TYPE_CHOICES)
+        return [
+            response_type_dict[response_type]
+            for response_type in self.response_type_values()
+        ]
 
     @property
     def redirect_uris(self):
--- oidc_provider/tests/app/utils.py
+++ oidc_provider/tests/app/utils.py
@@ -15,8 +15,7 @@ from django.contrib.auth.models import User
 from oidc_provider.models import (
     Client,
     Code,
-    Token,
-    ResponseType)
+    Token)
 
 
 FAKE_NONCE = 'cb584e44c43ed6bd0bc2d9c7e242837d'
@@ -68,8 +67,8 @@ def create_fake_client(response_type, is_public=False, require_consent=True):
     if isinstance(response_type, ("".__class__, u"".__class__)):
         response_type = (response_type,)
     for value in response_type:
-        client.response_types.add(ResponseType.objects.get(value=value))
-
+        client.response_types.add(value)
+    client.save()
     return client
 
 
