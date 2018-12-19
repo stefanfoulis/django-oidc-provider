c63472b38fff9c143ead1e1b74a253f061961b64
Stefan Foulis <stefan@foulis.ch>
2018-12-19 17:12:52 +0100

    Allow altering expires_at dynamically
    Works for dynamically altering `expires_at` in `create_token`, `create_code` and `update_or_create_user_consent`.

--- oidc_provider/lib/endpoints/token.py
+++ oidc_provider/lib/endpoints/token.py
@@ -181,7 +181,7 @@ class TokenEndpoint(object):
             "access_token": token.access_token,
             "refresh_token": token.refresh_token,
             "token_type": "bearer",
-            "expires_in": settings.get("OIDC_TOKEN_EXPIRE"),
+            "expires_in": token.valid_for.seconds,
             "id_token": encode_id_token(id_token_dic, token.client),
         }
 
@@ -225,7 +225,7 @@ class TokenEndpoint(object):
             "access_token": token.access_token,
             "refresh_token": token.refresh_token,
             "token_type": "bearer",
-            "expires_in": settings.get("OIDC_TOKEN_EXPIRE"),
+            "expires_in": token.valid_for.seconds,
             "id_token": encode_id_token(id_token_dic, self.token.client),
         }
 
@@ -254,7 +254,7 @@ class TokenEndpoint(object):
         return {
             "access_token": token.access_token,
             "refresh_token": token.refresh_token,
-            "expires_in": settings.get("OIDC_TOKEN_EXPIRE"),
+            "expires_in": token.valid_for.seconds,
             "token_type": "bearer",
             "id_token": encode_id_token(id_token_dic, token.client),
         }
@@ -268,7 +268,7 @@ class TokenEndpoint(object):
 
         return {
             "access_token": token.access_token,
-            "expires_in": settings.get("OIDC_TOKEN_EXPIRE"),
+            "expires_in": token.valid_for.seconds,
             "token_type": "bearer",
             "scope": self.client._scope,
         }
