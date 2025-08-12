fc513b1e6fa3eecebb0d29a4916df7bf84e0a21c
Stefan Foulis <stefan@foulis.ch>
2025-08-12 14:28:07 +0000

    WIP encrypt

--- oidc_provider/lib/utils/token.py
+++ oidc_provider/lib/utils/token.py
@@ -224,6 +224,8 @@ def _get_token(raw_token, fieldname, client=None):
     hash_fieldname = '{}_hash'.format(fieldname)
     hashed_token = hash_token(raw_token)
     token = qs.get(**{hash_fieldname: hashed_token})
+    # TODO: decrypt token
+
     if getattr(token, fieldname) != raw_token:
         # Suspicious. Bad hash in database or hash collision attack.
         raise Token.DoesNotExist(
@@ -235,6 +237,14 @@ def _get_token(raw_token, fieldname, client=None):
 
 def get_by_access_token(access_token, client=None):
     return _get_token(raw_token=access_token, fieldname='access_token', client=client)
+
+
+def default_get_valid_access_token(access_token, client, request):
+    return get_by_access_token(access_token=access_token, client=client)
+
+
+def get_valid_access_token(**kwargs):
+    return settings.get('OIDC_GET_VALID_ACCESS_TOKEN', import_str=True)(**kwargs)
 
 
 def get_by_refresh_token(refresh_token, client=None):
