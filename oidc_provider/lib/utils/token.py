c73894c31575902bef9d7a998d8b356afb42e794
Stefan Foulis <stefan@foulis.ch>
2025-08-20 01:39:21 +0200

    Apply initial ruff formatting to config base

--- oidc_provider/lib/utils/token.py
+++ oidc_provider/lib/utils/token.py
@@ -1,25 +1,25 @@
-from datetime import timedelta
 import time
 import uuid
+from datetime import timedelta
 
 from Cryptodome.PublicKey.RSA import importKey
-from django.utils import dateformat, timezone
+from django.utils import dateformat
+from django.utils import timezone
 from jwkest.jwk import RSAKey as jwk_RSAKey
 from jwkest.jwk import SYMKey
 from jwkest.jws import JWS
 from jwkest.jwt import JWT
 
-from oidc_provider.lib.utils.common import get_issuer, run_processing_hook
-from oidc_provider.lib.claims import StandardScopeClaims
-from oidc_provider.models import (
-    Code,
-    RSAKey,
-    Token,
-)
 from oidc_provider import settings
+from oidc_provider.lib.claims import StandardScopeClaims
+from oidc_provider.lib.utils.common import get_issuer
+from oidc_provider.lib.utils.common import run_processing_hook
+from oidc_provider.models import Code
+from oidc_provider.models import RSAKey
+from oidc_provider.models import Token
 
 
-def create_id_token(token, user, aud, nonce='', at_hash='', request=None, scope=None):
+def create_id_token(token, user, aud, nonce="", at_hash="", request=None, scope=None):
     """
     Creates the id_token dictionary.
     See: http://openid.net/specs/openid-connect-core-1_0.html#IDToken
@@ -27,43 +27,43 @@ def create_id_token(token, user, aud, nonce='', at_hash='', request=None, scope=
     """
     if scope is None:
         scope = []
-    sub = settings.get('OIDC_IDTOKEN_SUB_GENERATOR', import_str=True)(user=user)
+    sub = settings.get("OIDC_IDTOKEN_SUB_GENERATOR", import_str=True)(user=user)
 
-    expires_in = settings.get('OIDC_IDTOKEN_EXPIRE')
+    expires_in = settings.get("OIDC_IDTOKEN_EXPIRE")
 
     # Convert datetimes into timestamps.
     now = int(time.time())
     iat_time = now
     exp_time = int(now + expires_in)
     user_auth_time = user.last_login or user.date_joined
-    auth_time = int(dateformat.format(user_auth_time, 'U'))
+    auth_time = int(dateformat.format(user_auth_time, "U"))
 
     dic = {
-        'iss': get_issuer(request=request),
-        'sub': sub,
-        'aud': str(aud),
-        'exp': exp_time,
-        'iat': iat_time,
-        'auth_time': auth_time,
+        "iss": get_issuer(request=request),
+        "sub": sub,
+        "aud": str(aud),
+        "exp": exp_time,
+        "iat": iat_time,
+        "auth_time": auth_time,
     }
 
     if nonce:
-        dic['nonce'] = str(nonce)
+        dic["nonce"] = str(nonce)
 
     if at_hash:
-        dic['at_hash'] = at_hash
+        dic["at_hash"] = at_hash
 
     # Inlude (or not) user standard claims in the id_token.
-    if settings.get('OIDC_IDTOKEN_INCLUDE_CLAIMS'):
+    if settings.get("OIDC_IDTOKEN_INCLUDE_CLAIMS"):
         standard_claims = StandardScopeClaims(token)
         dic.update(standard_claims.create_response_dic())
-        if settings.get('OIDC_EXTRA_SCOPE_CLAIMS'):
-            custom_claims = settings.get('OIDC_EXTRA_SCOPE_CLAIMS', import_str=True)(token)
+        if settings.get("OIDC_EXTRA_SCOPE_CLAIMS"):
+            custom_claims = settings.get("OIDC_EXTRA_SCOPE_CLAIMS", import_str=True)(token)
             dic.update(custom_claims.create_response_dic())
 
     dic = run_processing_hook(
-        dic, 'OIDC_IDTOKEN_PROCESSING_HOOK',
-        user=user, token=token, request=request)
+        dic, "OIDC_IDTOKEN_PROCESSING_HOOK", user=user, token=token, request=request
+    )
 
     return dic
 
@@ -93,7 +93,7 @@ def client_id_from_id_token(id_token):
     Returns a string or None.
     """
     payload = JWT().unpack(id_token).payload()
-    aud = payload.get('aud', None)
+    aud = payload.get("aud", None)
     if aud is None:
         return None
     if isinstance(aud, list):
@@ -115,15 +115,15 @@ def create_token(user, client, scope, id_token_dic=None):
         token.id_token = id_token_dic
 
     token.refresh_token = uuid.uuid4().hex
-    token.expires_at = timezone.now() + timedelta(
-        seconds=settings.get('OIDC_TOKEN_EXPIRE'))
+    token.expires_at = timezone.now() + timedelta(seconds=settings.get("OIDC_TOKEN_EXPIRE"))
     token.scope = scope
 
     return token
 
 
-def create_code(user, client, scope, nonce, is_authentication,
-                code_challenge=None, code_challenge_method=None):
+def create_code(
+    user, client, scope, nonce, is_authentication, code_challenge=None, code_challenge_method=None
+):
     """
     Create and populate a Code object.
     Return a Code object.
@@ -138,8 +138,7 @@ def create_code(user, client, scope, nonce, is_authentication,
         code.code_challenge = code_challenge
         code.code_challenge_method = code_challenge_method
 
-    code.expires_at = timezone.now() + timedelta(
-        seconds=settings.get('OIDC_CODE_EXPIRE'))
+    code.expires_at = timezone.now() + timedelta(seconds=settings.get("OIDC_CODE_EXPIRE"))
     code.scope = scope
     code.nonce = nonce
     code.is_authentication = is_authentication
@@ -152,15 +151,15 @@ def get_client_alg_keys(client):
     Takes a client and returns the set of keys associated with it.
     Returns a list of keys.
     """
-    if client.jwt_alg == 'RS256':
+    if client.jwt_alg == "RS256":
         keys = []
         for rsakey in RSAKey.objects.all():
             keys.append(jwk_RSAKey(key=importKey(rsakey.key), kid=rsakey.kid))
         if not keys:
-            raise Exception('You must add at least one RSA Key.')
-    elif client.jwt_alg == 'HS256':
+            raise Exception("You must add at least one RSA Key.")
+    elif client.jwt_alg == "HS256":
         keys = [SYMKey(key=client.client_secret, alg=client.jwt_alg)]
     else:
-        raise Exception('Unsupported key algorithm.')
+        raise Exception("Unsupported key algorithm.")
 
     return keys
